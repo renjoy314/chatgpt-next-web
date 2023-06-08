8b9223cac53c5187c6dbb5725b6a7343957e53cc
renjoy314 <133527641+renjoy314@users.noreply.github.com>
2023-06-09 02:51:24 +0800

    Update en.ts

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -106,10 +106,10 @@ const en: RequiredLocaleType = {
     Update: {
       Version: (x: string) => `Version: ${x}`,
       IsLatest: "Latest version",
-      CheckUpdate: "Check Update",
-      IsChecking: "Checking update...",
-      FoundUpdate: (x: string) => `Found new version: ${x}`,
-      GoToUpdate: "Update",
+      CheckUpdate: "",
+      IsChecking: "",
+      FoundUpdate:
+      GoToUpdate: "",
     },
     SendKey: "Send Key",
     Theme: "Theme",
